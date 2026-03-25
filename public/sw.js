13518092a453348b7ddd90552baa8368afb27416
dinnertime.dev <dinnertime.dev@gmail.com>
2026-03-26 01:07:04 +0900

    pwa 되돌리기

--- public/sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener("fetch", (event) => {
-  // 이 내용이 비어있어도 좋으니 'fetch' 리스너 자체가 있어야 합니다!
-});
--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -1,13 +1,9 @@
 import { BottomMenuNavigator } from "@/components/page/bottom-menu-navigator";
-import { PWAInstall } from "@/components/ui/pwa-install";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="sm:max-w-md mx-auto shadow-lg bg-background relative">
-      <div className="fixed top-0 sm:max-w-md mx-auto w-full h-16 z-10">
-        <PWAInstall />
-      </div>
-      <main className="pb-16 pt-16 min-h-svh">{children}</main>
+      <main className="pb-16 min-h-svh">{children}</main>
       <BottomMenuNavigator className="fixed bottom-0 sm:max-w-md mx-auto w-full h-16" />
     </div>
   );
