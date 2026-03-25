b55b6159bff3b9e3a9ac40475a60bcf0ee5b5e13
dinnertime.dev <dinnertime.dev@gmail.com>
2026-03-25 23:49:02 +0900

    force-dynamic 적용

--- src/app/(platform)/page.tsx
+++ src/app/(platform)/page.tsx
@@ -1,5 +1,7 @@
 import { FirstSection } from "@/components/page/main/first-section";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <div className="relative">
