d286115d469322eaa23c2602720fbde8e00df3cb
dinnertime.dev <dinnertime.dev@gmail.com>
2026-03-25 23:46:43 +0900

    디버깅을 위한 console remove false

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  compiler: {
+    removeConsole: false,
+  },
 };
 
 export default nextConfig;
