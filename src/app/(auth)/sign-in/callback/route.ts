d479f6e7d5fc42836ffce681c59e8e4f47d4a822
dinnertime.dev <dinnertime.dev@gmail.com>
2026-03-26 00:03:50 +0900

    site url 변경

--- src/app/(auth)/sign-in/callback/route.ts
+++ src/app/(auth)/sign-in/callback/route.ts
@@ -2,13 +2,11 @@ import { type NextRequest, NextResponse } from "next/server";
 import { clearPendingRecordAction, getPendingRecordAction } from "@/actions/auth/get-pending-record.action";
 import { saveGalpiAction } from "@/actions/galpi/save-galpi.action";
 import { getSession } from "@/actions/utils";
+import { env } from "@/config/env";
 
 export async function GET(request: NextRequest) {
-  const origin = request.nextUrl.origin;
-  console.log(origin);
-
   const session = await getSession();
-  if (!session) return NextResponse.redirect(new URL("/sign-in", origin));
+  if (!session) return NextResponse.redirect(new URL("/sign-in", env.SITE_URL));
 
   const { result: pending } = await getPendingRecordAction();
 
